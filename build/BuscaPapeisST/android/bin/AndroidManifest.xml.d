281cf6ad046e0e3ab16750f4165a4c50bb7cca33
diegodukao <diegodukao@gmail.com>
2013-04-17 10:27:10 -0300

    removing bin files from repo and adding to .gitignore

--- build/BuscaPapeisST/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/diego/Projects/propus/BuscaPapeisST/build/BuscaPapeisST/android/bin/AndroidManifest.xml : \
-/home/diego/Projects/propus/BuscaPapeisST/build/BuscaPapeisST/android/AndroidManifest.xml \
